525be49a90d75a21eb0f9f12df6c7c24f07c545b
dndanli <danielkcode@gmail.com>
2021-11-21 16:49:33 -0600

    sign-up page update

--- cantus-music/src/Pages/Registration.tsx
+++ cantus-music/src/Pages/Registration.tsx
@@ -1,7 +1,66 @@
 import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type FormValues = {
+  email: string;
+  password: string;
+  userName: string;
+};
 
 const Registration: React.FC = () => {
-  return <div className="Registration">This is the Registration page</div>;
+  const { register, handleSubmit, formState:{errors}} = useForm<FormValues>();
+
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    console.log(data);
+  };
+
+  let emailPattern = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
+
+  return (
+    <div className="registration">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="reg-questions">
+            <p>What's your email?</p>
+          </div>
+          <input
+            type="text"
+            placeholder="email"
+            {...register("email", { required: true, pattern: emailPattern })}
+          />
+
+          {errors.email && <i>Please enter a valid email format</i>}
+          <div className="reg-questions">
+            <p>Crate a password.</p>
+          </div>
+
+          <input
+            type="password"
+            placeholder="password"
+            {...(register("password"),
+            { required: true, minLength: 8, maxLength: 24 })}
+          />
+
+          {errors.password && <i> Password minimum length is 8 max is 24</i>}
+
+          <div className="reg-questions">
+            <p>What do you want to be called?</p>
+          </div>
+          <input
+            type="text"
+            placeholder="username"
+            {...register("userName", {
+              required: true,
+              minLength: 5,
+              maxLength: 12,
+            })}
+          />
+
+          {errors.userName && <i>Username minimum length is 5 max is 12</i>}
+
+          <input type="submit" className="submit-btn" />
+        </form>
+    </div>
+  );
 };
 
 export default Registration;
--- cantus-music/src/index.tsx
+++ cantus-music/src/index.tsx
@@ -38,6 +38,55 @@ const GlobalStyle = createGlobalStyle`
     text-decoration: none;
     color:rgb(255, 254, 252);
   }
+
+  .registration{
+    width: 100%;
+    height: 100vh;
+    p{
+      font-family: Montserrat, sans-serif;
+    }
+  }
+
+  form{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 5rem;
+  }
+
+  input{
+    border: none;
+    appearance: none;
+    background: rgb(231, 229, 226);
+    padding: .5rem;
+    border-radius: .2rem;
+    width: 55%;
+    margin: 8px;
+    box-sizing: border-box;
+    font-family: Montserrat, sans-serif;
+    font-size: 1rem;
+  }
+  .submit-btn{
+    color:rgb( 255, 254, 252);
+    font-size: 1rem;
+    background: #f9b026;
+    font-family: Montserrat, sans-serif;
+    font-weight: 500;
+  }
+  
+  input:focus::placeholder{
+    color: transparent;
+  }
+
+  input::placeholder{
+    color:#111111;
+    transition: color 0.3s ease;
+  }
+
+  .reg-questions{
+    width: 55%;
+  }
+
 `;
 
 ReactDOM.render(
