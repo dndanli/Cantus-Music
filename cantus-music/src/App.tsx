4f4009faa382d76a83b6394d260fde9d2d961101
dndanli <danielkcode@gmail.com>
2021-11-20 22:42:18 -0600

    routes update

--- cantus-music/src/App.tsx
+++ cantus-music/src/App.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import "./App.css";
 import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <Home />
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
