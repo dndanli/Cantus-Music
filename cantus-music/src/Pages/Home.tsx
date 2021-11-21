26a093a6db881b94bffbe4edd2a82f7e3b8e1fde
dndanli <danielkcode@gmail.com>
2021-11-20 21:58:28 -0600

    home.tsx and index.tsx update

--- cantus-music/src/Pages/Home.tsx
+++ cantus-music/src/Pages/Home.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { ReactComponent as MainIllustration } from "../Assets/Images/undraw_music_re_a2jk.svg";
 
 const Home: React.FC = () => {
-  return <div className="Home">This is the Home page</div>;
+  return (
+    <div>
+      <main className="hero">
+        <div className="hero-illustration">
+          <MainIllustration width="250" height="250" />
+        </div>
+        <div className="heading">
+          <h1>Music is Everything</h1>
+        </div>
+        <div className="info">
+          <h3>Listen to your favorite songs with Cantus Music</h3>
+        </div>
+        <div className="get-started">
+          <h3>Try Cantus free</h3>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default Home;
--- cantus-music/src/index.tsx
+++ cantus-music/src/index.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createGlobalStyle } from "styled-components";
+import App from "./App";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: rgb(255, 254, 252);
+  }
+`;
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
