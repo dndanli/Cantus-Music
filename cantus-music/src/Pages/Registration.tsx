6c3c0b9847c24a1081848e3aa035b4bbfad4e9ed
dndanli <danielkcode@gmail.com>
2021-11-22 22:19:24 -0600

    code format update

--- cantus-music/src/Pages/Registration.tsx
+++ cantus-music/src/Pages/Registration.tsx
@@ -9,7 +9,11 @@ type FormValues = {
 };
 
 const Registration: React.FC = () => {
-  const { register, handleSubmit, formState:{errors}} = useForm<FormValues>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>();
 
   const onSubmit: SubmitHandler<FormValues> = (data) => {
     console.log(data);
@@ -19,50 +23,52 @@ const Registration: React.FC = () => {
 
   return (
     <div className="registration">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="reg-questions">
-            <p>What's your email?</p>
-          </div>
-          <input
-            type="text"
-            placeholder="email"
-            {...register("email", { required: true, pattern: emailPattern })}
-          />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="reg-questions">
+          <p>What's your email?</p>
+        </div>
+        <input
+          type="text"
+          placeholder="email"
+          {...register("email", { required: true, pattern: emailPattern })}
+        />
 
-          {errors.email && <i>Please enter a valid email format</i>}
-          <div className="reg-questions">
-            <p>Crate a password.</p>
-          </div>
+        {errors.email && <i>Please enter a valid email format</i>}
+        <div className="reg-questions">
+          <p>Crate a password.</p>
+        </div>
 
-          <input
-            type="password"
-            placeholder="password"
-            {...(register("password"),
-            { required: true, minLength: 8, maxLength: 24 })}
-          />
+        <input
+          type="password"
+          placeholder="password"
+          {...(register("password"),
+          { required: true, minLength: 8, maxLength: 24 })}
+        />
 
-          {errors.password && <i> Password minimum length is 8 max is 24</i>}
+        {errors.password && <i> Password minimum length is 8 max is 24</i>}
 
-          <div className="reg-questions">
-            <p>What do you want to be called?</p>
-          </div>
-          <input
-            type="text"
-            placeholder="username"
-            {...register("userName", {
-              required: true,
-              minLength: 5,
-              maxLength: 12,
-            })}
-          />
+        <div className="reg-questions">
+          <p>What do you want to be called?</p>
+        </div>
+        <input
+          type="text"
+          placeholder="username"
+          {...register("userName", {
+            required: true,
+            minLength: 5,
+            maxLength: 12,
+          })}
+        />
 
-          {errors.userName && <i>Username minimum length is 5 max is 12</i>}
+        {errors.userName && <i>Username minimum length is 5 max is 12</i>}
 
-          <input type="submit" className="submit-btn" />
-          <div className="reg-questions">
-            <p>Already have an account? <Link to="/login">Log in</Link></p>
-          </div>
-        </form>
+        <input type="submit" className="submit-btn" />
+        <div className="reg-questions">
+          <p>
+            Already have an account? <Link to="/login">Log in</Link>
+          </p>
+        </div>
+      </form>
     </div>
   );
 };
