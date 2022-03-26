5f92707d032c26347312585c4f769d1f2957ef41
Daniel De Lima <danielkcode@gmail.com>
2022-03-26 17:48:53 -0500

    added search bar component style

---
+++ cantus-music/src/Components/Layout/SearchBar/SearchBar.style.ts
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+import SearchBar from "./SearchBar";
+
+const StyledSearchBar = styled(SearchBar)`
+  background-color: rgb(245, 245, 245);
+  min-height: 2rem;
+  padding-block: 0.5rem;
+  border-radius: 8px;
+
+  .search-icon,
+  .close-icon,
+  .close-icon-vis {
+    font-size: 2rem;
+    color: rgba(16, 16, 16, 0.7);
+  }
+
+  .search-icon {
+    padding-left: 10px;
+  }
+
+  .close-icon {
+    visibility: hidden;
+  }
+
+  .close-icon-vis {
+    padding-right: 10px;
+  }
+
+  display: flex;
+  .search-field {
+    width: 100%;
+    max-width: 100%;
+    min-width: 20px;
+    outline: none;
+    border: none;
+    font-size: 16px;
+    padding-left: 10px;
+    padding-right: 10px;
+    background-color: inherit;
+  }
+  input[type="search"]::-webkit-search-decoration,
+  input[type="search"]::-webkit-search-cancel-button,
+  input[type="search"]::-webkit-search-results-button,
+  input[type="search"]::-webkit-search-results-decoration {
+    -webkit-appearance: none;
+  }
+`;
+
+export default StyledSearchBar;
