d4f16cd7a449ad5fa3edb1602f6793b4e6cd5899
Daniel De Lima <danielkcode@gmail.com>
2022-03-26 17:49:45 -0500

    imported and added search page component to App.tsx

--- cantus-music/src/App.tsx
+++ cantus-music/src/App.tsx
@@ -4,6 +4,8 @@ import Registration from "./Pages/Registration";
 import Login from "./Pages/Login";
 import Discover from "./Pages/Discover";
 import Library from "./Pages/Library";
+import StyledSearch from "./Pages/Search.style";
+
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App: React.FC = () => {
@@ -16,6 +18,10 @@ const App: React.FC = () => {
           <Route path="/login" element={<Login />}></Route>
           <Route path="/discover" element={<Discover />}></Route>
           <Route path="/library" element={<Library />}></Route>
+          <Route
+            path="/search"
+            element={<StyledSearch className="search-page" />}
+          ></Route>
         </Routes>
       </BrowserRouter>
     </div>
