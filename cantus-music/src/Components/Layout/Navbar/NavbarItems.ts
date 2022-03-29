70ded920fed9713786c53f2ce5df4ed80e61bfb6
Daniel De Lima <danielkcode@gmail.com>
2022-03-29 15:12:22 -0500

    changed icon

--- cantus-music/src/Components/Layout/Navbar/NavbarItems.ts
+++ cantus-music/src/Components/Layout/Navbar/NavbarItems.ts
@@ -1,9 +1,10 @@
 import { IconType } from "react-icons";
 import { BsSearch, BsCloudDownload } from "react-icons/bs";
-import { AiOutlineHome, AiOutlineMenu } from "react-icons/ai";
+import { AiOutlineMenu } from "react-icons/ai";
+import { RiHome2Line } from "react-icons/ri";
 
 export let NavItems: { id: number; text: string; icon: IconType }[] = [
-  { id: 0, text: "discover", icon: AiOutlineHome },
+  { id: 0, text: "discover", icon: RiHome2Line },
   { id: 1, text: "search", icon: BsSearch },
   { id: 2, text: "library", icon: AiOutlineMenu },
   { id: 3, text: "download", icon: BsCloudDownload },
