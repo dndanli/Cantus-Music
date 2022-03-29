ad032e9033c0c78420fca442180cf3e0da5e055b
Daniel De Lima <danielkcode@gmail.com>
2022-03-29 15:12:02 -0500

    changed bg color

--- cantus-music/src/Components/Layout/Navbar/Navbar.style.ts
+++ cantus-music/src/Components/Layout/Navbar/Navbar.style.ts
@@ -4,7 +4,12 @@ import Navbar from "./Navbar";
 const StyledNavbar = styled(Navbar)`
   border-radius: 1rem;
   width: 90%;
-  background-color: #3a1d5c;
+  background-color: #1a1c21;
+  background-image: linear-gradient(
+    45deg,
+    rgba(26, 28, 33, 1),
+    rgba(33, 35, 40, 0.8)
+  );
   height: 4.5rem;
   .nav-ul {
     padding: 1rem 0 0 0;
