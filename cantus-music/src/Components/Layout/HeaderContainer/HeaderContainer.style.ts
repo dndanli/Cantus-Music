83b1b60401c5ff80edcea62bef3c3ed157a18bc2
Daniel De Lima <danielkcode@gmail.com>
2022-03-29 15:10:44 -0500

    updated styling

--- cantus-music/src/Components/Layout/HeaderContainer/HeaderContainer.style.ts
+++ cantus-music/src/Components/Layout/HeaderContainer/HeaderContainer.style.ts
@@ -2,28 +2,15 @@ import styled from "styled-components";
 import HeaderContainer from "./HeaderContainer";
 
 const StyledHeaderContainer = styled(HeaderContainer)`
-  background-color: rgb(255, 254, 252);
   display: flex;
-  justify-content: space-evenly;
-  padding-top: 0.5rem;
-  align-items: center;
+  padding-block: 0.5rem;
+  align-items: flex-start;
 
   .logo {
-    margin-left: -6.2rem;
-    /* TODO: change font */
+    padding-left: 1rem;
     font-family: Montserrat, sans-serif;
     text-decoration: none;
-    color: #c293eb;
-  }
-
-  .listen-in-app-btn {
-    background-color: #3a1d5c;
-    border-radius: 0.5rem;
-    color: rgb(255, 254, 252);
-    padding: 0.8rem;
-    margin-right: -5rem;
-    font-size: 10px;
-    font-family: Montserrat, sans-serif;
+    color: rgba(255, 183, 0, 0.9);
   }
 `;
 
