34d95f200b00b1c9b172c6df34bf8f1545668de7
Daniel De Lima <danielkcode@gmail.com>
2022-03-29 15:11:34 -0500

    styling update

--- cantus-music/src/Components/Layout/LinkContainer/LinkContainer.style.ts
+++ cantus-music/src/Components/Layout/LinkContainer/LinkContainer.style.ts
@@ -5,7 +5,7 @@ export const StyledLinkContainer = styled(LinkContainer)`
   display: flex;
   justify-content: space-around;
   padding: 0.5rem;
-  background-color: rgba(58, 29, 92, 0.9);
+  background-color: #2a2c30;
   border-radius: 0.5rem;
   color: rgb(255, 254, 252);
   margin-bottom: 0.5rem;
@@ -13,7 +13,7 @@ export const StyledLinkContainer = styled(LinkContainer)`
   h3 {
     margin-right: auto;
   }
-  h4 {
-    color: #c293eb;
+  .link-circle-icon {
+    color: rgba(255, 183, 0, 0.9);
   }
 `;
