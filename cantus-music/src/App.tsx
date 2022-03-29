b3ee53804ccec61d8d1ef69809575dfc62722486
Daniel De Lima <danielkcode@gmail.com>
2022-03-29 15:14:09 -0500

    defined new routes

--- cantus-music/src/App.tsx
+++ cantus-music/src/App.tsx
@@ -3,8 +3,9 @@ import Home from "./Pages/Home";
 import Registration from "./Pages/Registration";
 import Login from "./Pages/Login";
 import Discover from "./Pages/Discover";
-import Library from "./Pages/Library";
+import StyledLibrary from "./Pages/Library.style";
 import StyledSearch from "./Pages/Search.style";
+import StyledUserProfile from "./Pages/UserProfile.style";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
@@ -17,10 +18,17 @@ const App: React.FC = () => {
           <Route path="/signup" element={<Registration />}></Route>
           <Route path="/login" element={<Login />}></Route>
           <Route path="/discover" element={<Discover />}></Route>
-          <Route path="/library" element={<Library />}></Route>
+          <Route
+            path="/library"
+            element={<StyledLibrary className="library-page" />}
+          ></Route>
           <Route
             path="/search"
             element={<StyledSearch className="search-page" />}
+          ></Route>
+          <Route
+            path="username:1"
+            element={<StyledUserProfile className="user-profile" />}
           ></Route>
         </Routes>
       </BrowserRouter>
