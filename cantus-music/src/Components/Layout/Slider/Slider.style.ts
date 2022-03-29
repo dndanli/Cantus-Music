902a910bf1dccbded134fafe2554644650eeb983
Daniel De Lima <danielkcode@gmail.com>
2022-03-29 18:08:00 -0500

    made height and width optional

--- cantus-music/src/Components/Layout/Slider/Slider.style.ts
+++ cantus-music/src/Components/Layout/Slider/Slider.style.ts
@@ -2,14 +2,13 @@ import styled from "styled-components";
 import Slider from "./Slider";
 
 type SliderProps = {
-  height: string;
-  width: string;
+  height?: string;
+  width?: string;
 };
 
 export const StyledSlider = styled(Slider)<SliderProps>`
   height: ${(props) => props.height};
   width: ${(props) => props.width};
-
   overflow: auto;
   white-space: nowrap;
 
