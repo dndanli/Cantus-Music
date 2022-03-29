c45476e46a0554ce64aac96d6f4b06eaec79c5a2
Daniel De Lima <danielkcode@gmail.com>
2022-03-29 15:10:11 -0500

    removed button from header container

--- cantus-music/src/Components/Layout/HeaderContainer/HeaderContainer.tsx
+++ cantus-music/src/Components/Layout/HeaderContainer/HeaderContainer.tsx
@@ -10,10 +10,6 @@ const HeaderContainer: React.FC<HeaderProps> = ({ className }: HeaderProps) => {
       <a href="/" className="logo">
         <h2>Cantus</h2>
       </a>
-
-      <div className="listen-in-app-btn">
-        <h2>Listen in app</h2>
-      </div>
     </div>
   );
 };
