c4740e41c960ec1eaecda6208ebb1a8b3e88a4dd
Daniel De Lima <danielkcode@gmail.com>
2022-03-29 15:03:59 -0500

    added song track component style

---
+++ cantus-music/src/Components/Layout/SongTrack/SongTrack.style.ts
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import SongTrack from "./SongTrack";
+
+const StyledSongTrack = styled(SongTrack)`
+  display: flex;
+  font-family: "Montserrat", sans-serif;
+  .track-title {
+    color: rgba(16, 16, 16, 0.9);
+  }
+  .song-track-info {
+    margin-left: 1rem;
+    width: 100%;
+    font-size: 13px;
+  }
+  .track-metadata {
+    width: 100%;
+    margin-top: 0.5rem;
+    display: flex;
+    justify-content: flex-start;
+  }
+  .number-of-plays {
+    margin-right: 1rem;
+  }
+  .track-img {
+    max-width: 120px;
+    max-height: 120px;
+  }
+`;
+
+export default StyledSongTrack;
