dd06693a3cf180d118ac1a243cb2d5456182f3c1
Daniel De Lima <danielkcode@gmail.com>
2022-03-29 15:08:48 -0500

    Library page icon and div update

--- cantus-music/src/Pages/Library.tsx
+++ cantus-music/src/Pages/Library.tsx
@@ -1,17 +1,30 @@
-import React from "react";
 import StyledHeaderContainer from "../Components/Layout/HeaderContainer/HeaderContainer.style";
 import StyledNavbar from "../Components/Layout/Navbar/Navbar.style";
 import { StyledLinkContainer } from "../Components/Layout/LinkContainer/LinkContainer.style";
+import { AiOutlineUser } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
-const Library: React.FC = () => {
+type LibraryProps = {
+  className: string;
+};
+
+const Library = ({ className }: LibraryProps) => {
   return (
-    <div>
+    <div className={className}>
       <div className="header-wrapper">
         <StyledHeaderContainer className="header-container" />
       </div>
       <div className="library">
         <div className="user-menu">
-          <h2 style={{ marginBottom: "1rem" }}>My Library</h2>
+          <div className="user-info-wrapper">
+            <h2 className="lib-header-2">My Library</h2>
+
+            <div className="user-icon">
+              <Link to="/username:1">
+                <AiOutlineUser id="user-icon" />
+              </Link>
+            </div>
+          </div>
           <StyledLinkContainer className="link-container" linkName="Liked" />
           <StyledLinkContainer
             className="link-container"
